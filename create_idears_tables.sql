492ca5862f3b45f9dfa347ba34dd0c77676258cb
Karsten Eckhardt <karsten.eckhardt@gmail.com>
2019-10-26 20:31:42 +0700

    Preparing to use Auropra Serverless for ideas and collections, instead of DynamoDB

--- create_idears_tables.sql
+++ create_idears_tables.sql
@@ -1,21 +1,21 @@
 CREATE TABLE `sidears`.`idears` (
-  `idea_id` VARCHAR(50) NOT NULL,
+  `idea_id` VARCHAR(255) NOT NULL,
   `idea` MEDIUMTEXT NOT NULL,
-  `created_at` DATETIME NULL,
+  `created_at` DATETIME NOT NULL,
   `img_link` VARCHAR(255) NULL,
   `upvote_count` INT NULL,
-  `user_name` VARCHAR(100) NOT NULL,
+  `username` VARCHAR(100) NOT NULL,
   PRIMARY KEY (`idea_id`));
 
 CREATE TABLE `sidears`.`tags` (
-  `tag_id` VARCHAR(50) NOT NULL,
-  `name` VARCHAR(50) NOT NULL,
+  `tag_id` VARCHAR(255) NOT NULL,
+  `name` VARCHAR(100) NOT NULL,
   `created_at` DATETIME NULL,
   PRIMARY KEY (`tag_id`));
 
 CREATE TABLE `sidears`.`idea-tags` (
-  `idea_id` VARCHAR(50) NOT NULL,
-  `tag_id` VARCHAR(50) NULL,
+  `idea_id` VARCHAR(255) NOT NULL,
+  `tag_id` VARCHAR(255) NULL,
   PRIMARY KEY (`idea_id`),
   INDEX `fk_tag_idx` (`tag_id` ASC),
   CONSTRAINT `fk_idea`
@@ -29,3 +29,28 @@ CREATE TABLE `sidears`.`idea-tags` (
     ON DELETE NO ACTION
     ON UPDATE NO ACTION);
 
+CREATE TABLE `sidears`.`collections` (
+    `collection_id` VARCHAR(255),
+    `description` MEDIUMTEXT,
+    `idea_id` VARCHAR(50) NOT NULL,
+    `username` VARCHAR(50) NOT NULL,
+    `created_at` DATETIME NOT NULL,
+    INDEX (`username`),
+    PRIMARY KEY (`collection_id`));
+
+CREATE TABLE `sidears`.`idea-collection` (
+    `idea_id` VARCHAR(255) NOT NULL,
+    `collection_id` VARCHAR(255) NULL,
+    PRIMARY KEY (`idea_id`),
+    INDEX `fk_tag_idx` (`collection_id` ASC),
+    CONSTRAINT `fk_idea-origin`
+        FOREIGN KEY (`idea_id`)
+            REFERENCES `sidears`.`idears` (`idea_id`)
+            ON DELETE NO ACTION
+            ON UPDATE NO ACTION,
+    CONSTRAINT `fk_collection`
+        FOREIGN KEY (`collection_id`)
+            REFERENCES `sidears`.`collections` (`collection_id`)
+            ON DELETE NO ACTION
+            ON UPDATE NO ACTION
+)
